2197959651e6957610f2e2d94604c2ff29cb064d
Khanh Vuong <vuong.van.khanh@alliedtechbase.com>
2019-10-10 08:49:49 +0700

    finish feature

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<base href="<?php echo base_url();?>" />
-	<title><?php echo $this->config->item('company') . ' | OSPOS ' . $this->config->item('application_version')  . ' | ' .  $this->lang->line('login_login'); ?></title>
+	<title><?php echo $this->config->item('company') . ' | KIOTAZ ' . $this->config->item('application_version')  . ' | ' .  $this->lang->line('login_login'); ?></title>
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="<?php echo 'dist/bootswatch/' . (empty($this->config->item('theme')) ? 'flatly' : $this->config->item('theme')) . '/bootstrap.min.css' ?>"/>
 	<!-- start css template tags -->
@@ -12,43 +12,41 @@
 </head>
 
 <body>
-	<div id="logo" align="center"><img src="<?php echo base_url();?>/images/logo.png"></div>
+    <div class="bg">
+        <div id="logo" align="center"><img src="<?php echo base_url();?>images/logo.svg"></div>
 
-	<div id="login">
-		<?php echo form_open('login') ?>
-			<div id="container">
-				<div align="center" style="color:red"><?php echo validation_errors(); ?></div>
+        <div id="login">
+            <?php echo form_open('login') ?>
+            <div id="container">
+                <div align="center" style="color:red"><?php echo validation_errors(); ?></div>
 
-				<?php if (!$this->migration->is_latest()): ?>
-				<div align="center" style="color:red"><?php echo $this->lang->line('common_migration_needed', $this->config->item('application_version')); ?></div>
-				<?php endif; ?>
+                <div id="login_form">
+                    <div class="input-group">
+                        <span class="input-group-addon input-sm"><span class="glyphicon glyphicon-user"></span></span>
+                        <input class="form-control" placeholder="<?php echo $this->lang->line('login_username')?>" name="username" type="username" size=20 autofocus></input>
+                    </div>
 
-				<div id="login_form">
-					<div class="input-group">
-						<span class="input-group-addon input-sm"><span class="glyphicon glyphicon-user"></span></span>
-						<input class="form-control" placeholder="<?php echo $this->lang->line('login_username')?>" name="username" type="username" size=20 autofocus></input>
-					</div>
+                    <div class="input-group">
+                        <span class="input-group-addon input-sm"><span class="glyphicon glyphicon-lock"></span></span>
+                        <input class="form-control" placeholder="<?php echo $this->lang->line('login_password')?>" name="password" type="password" size=20></input>
+                    </div>
 
-					<div class="input-group">
-						<span class="input-group-addon input-sm"><span class="glyphicon glyphicon-lock"></span></span>
-						<input class="form-control" placeholder="<?php echo $this->lang->line('login_password')?>" name="password" type="password" size=20></input>
-					</div>
+                    <?php
+                    if($this->config->item('gcaptcha_enable'))
+                    {
+                        echo '<script src="https://www.google.com/recaptcha/api.js"></script>';
+                        echo '<div class="g-recaptcha" align="center" data-sitekey="' . $this->config->item('gcaptcha_site_key') . '"></div>';
+                    }
+                    ?>
 
-					<?php
-					if($this->config->item('gcaptcha_enable'))
-					{
-						echo '<script src="https://www.google.com/recaptcha/api.js"></script>';
-						echo '<div class="g-recaptcha" align="center" data-sitekey="' . $this->config->item('gcaptcha_site_key') . '"></div>';
-					}
-					?>
+                    <input class="btn btn-primary btn-block" type="submit" name="loginButton" value="Go"/>
+                </div>
+            </div>
+            <?php echo form_close(); ?>
 
-					<input class="btn btn-primary btn-block" type="submit" name="loginButton" value="Go"/>
-				</div>
-			</div>
-		<?php echo form_close(); ?>
+        </div>
+    </div>
 
-		<h1>Open Source Point Of Sale <?php echo $this->config->item('application_version'); ?></h1>
-
-	</div>
+    <?php $this->load->view('partial/3rd_party_services') ?>
 </body>
 </html>
